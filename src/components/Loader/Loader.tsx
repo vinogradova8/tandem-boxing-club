e626313572da788890b584e4cd329880231719af
Viktoriia Vynohradova <viktoriia.vynohradova8@gmail.com>
2024-09-05 17:12:09 +0300

    faq

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -5,16 +5,17 @@ import './Loader.scss';
 export const Loader: React.FC = () => {
   return (
     <div className="loaders">
-      <video
+      {/* <video
         className="loaders__image"
-        src="https://vinogradova8.github.io/boxing/images/loader/stars.mp4"
-      ></video>
-      {/* <img
+        src="https://vinogradova8.github.io/boxing/images/loader/stars.gif"
+      ></video> */}
+      <img
         className="loaders__image"
+        // src="../images/loader/stars.gif"
         src="https://vinogradova8.github.io/boxing/images/loader/stars.gif"
         // src="https://vinogradova8.github.io/boxing/images/loader/spinner-transparent.gif"
         alt="Loading"
-      /> */}
+      />
     </div>
   );
 };

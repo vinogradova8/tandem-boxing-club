6a9e0b59a8842d163ef0440b838ec77835865b20
Viktoriia Vynohradova <viktoriia.vynohradova8@gmail.com>
2024-11-12 13:22:49 +0200

    google authentication

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -83,8 +83,8 @@ export const Login: React.FC = ({}) => {
             <div className="login__google">
               <a
                 className="login__google-link"
-                href="http://localhost:8088/login/oauth2/authorization/google"
-                // href="https://d1g3i7mr74vp7j.cloudfront.net/login/oauth2/authorization/google"
+                // href="http://localhost:8088/login/oauth2/authorization/google"
+                href="https://d1g3i7mr74vp7j.cloudfront.net/login/oauth2/authorization/google"
               >
                 {t('Continue with')}
                 <div className="login__google-icon"></div>

895e46b448fba7fd29207ab6615a5a0f0b151b7f
Viktoriia Vynohradova <viktoriia.vynohradova8@gmail.com>
2024-11-24 19:54:43 +0200

    fixed questions

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const gallery = axios.create({
-  baseURL: 'https://vinogradova8.github.io/boxing/api',
+  baseURL: 'https://vinogradova8.github.io/tandem-boxing-club/api',
 });
